fc5eea3f9b7d43f71d135c217d3f003a7e8ee362
Yuasito <joshsuper38@gmail.com>
2025-10-04 11:00:33 +0800

    GitCommands.java

--- GitCommands.java
+++ GitCommands.java
@@ -28,14 +28,14 @@ class GitCommands {
 
         int total = allChanges.size();
     
-        StringBuilder sb = new StringBuilder();
-        sb.append("You have ").append(total).append(" change/s.\n");
+        StringBuilder josh = new StringBuilder();
+        josh.append("You have ").append(total).append(" change/s.\n");
 
         for (String path : allChanges) {
-        sb.append(path).append("\n");
+        josh.append(path).append("\n");
         }
 
-        return sb.toString();
+        return josh.toString();
     }
 
     // Command: git add <filename/file directory/wildcard>
